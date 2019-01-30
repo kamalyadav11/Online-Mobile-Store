59c38c17097f468b6f7e4a69f5e3d4fe43ba7cc2
kamal11 <kyzx11@gmail.com>
2019-01-30 19:55:03 +0530

    added font-awesome in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-6">column number 1</div>
-          <div className="col-6">column number 2</div>
+          <div className="col-6">
+            <span>
+              <i className="fas fa-home" />
+            </span>
+          </div>
         </div>
       </div>
     );
