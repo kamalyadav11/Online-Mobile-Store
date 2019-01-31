26b78090c30960a75ee38fb649a62a42d42af76c
kamal11 <kyzx11@gmail.com>
2019-01-31 21:12:12 +0530

    added title and image of phone

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 export default class Details extends Component {
   render() {
-    return <div>Hello from details</div>;
+    return (
+      <ProductConsumer>
+        {value => {
+          const {
+            id,
+            img,
+            company,
+            info,
+            price,
+            title,
+            inCart
+          } = value.detailProduct;
+          return (
+            <div className="container py-5">
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <img src={img} className="img-fluid" alt="product" />
+                </div>
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize" />
+              </div>
+            </div>
+          );
+        }}
+      </ProductConsumer>
+    );
   }
 }
