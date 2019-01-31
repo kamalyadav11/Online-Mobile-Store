f72f7459881922db7c9341ec935aeae9c6613608
kamal11 <kyzx11@gmail.com>
2019-01-31 14:23:10 +0530

    organized imports

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
+import { ProductConsumer } from "../context";
 import Product from "./Product";
 import Title from "./Title";
-import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
   render() {
