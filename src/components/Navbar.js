8770d212da10ab821fd253ca14ec21d50faef338
kamal11 <kyzx11@gmail.com>
2019-01-31 13:27:24 +0530

    Added separate Button.js file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 import logo from "../logo.svg";
 
 export default class Navbar extends Component {
@@ -29,23 +29,3 @@ export default class Navbar extends Component {
     );
   }
 }
-
-const ButtonContainer = styled.button`
-  text-transform: capitalize;
-  font-size: 1.4rem;
-  background: transparent;
-  border: 0.05rem solid var(--lightBlue);
-  color: var(--lightBlue);
-  border-radius: 0.5rem;
-  padding: 0.2rem 0.5rem;
-  cursor: pointer;
-  margin: 0.2rem 0.5rem 0.2rem 0;
-  transition: all 0.5s ease-in-out;
-  &:hover {
-    background: var(--lightBlue);
-    color: var(--mainBlue);
-  }
-  &:focus {
-    outline: none;
-  }
-`;
