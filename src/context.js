fad5387458c97b9e0e2645ff2158ed5b54359af1
kamal11 <kyzx11@gmail.com>
2019-01-31 14:12:36 +0530

    added products in context state

--- src/context.js
+++ src/context.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
 
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct
+  };
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider value={{ ...this.state }}>
         {this.props.children}
       </ProductContext.Provider>
     );
