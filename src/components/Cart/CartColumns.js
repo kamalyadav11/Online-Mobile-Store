531bbe5e2f25200e508383fc3208439ad91ebdb1
kamal11 <kyzx11@gmail.com>
2019-02-02 16:20:31 +0530

    layout bug fixed

---
+++ src/components/Cart/CartColumns.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const CartColumns = () => {
+  return (
+    <div className="container-fuid text-center d-none d-lg-block">
+      <div className="row">
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Products</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Products</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Products</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Products</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Products</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Products</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartColumns;
