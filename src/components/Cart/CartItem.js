99f33f3c168fd1f43690b9bef05a9405066fa0e2
kamal11 <kyzx11@gmail.com>
2019-02-02 19:29:44 +0530

    rendered image in cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const CartItem = () => {
-  return <div>hello</div>;
+const CartItem = ({ item, value }) => {
+  const { id, title, img, price, total, count } = item;
+  const { increment, decrement } = value;
+  return (
+    <div className="row my-1 text-capitalize text-center">
+      <div className="col-10 mx-auto col-lg-2">
+        <img
+          src={img}
+          style={{ width: "5rem", height: "5rem" }}
+          className="img-fluid"
+          alt="product"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
