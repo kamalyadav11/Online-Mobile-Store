4efa67fc1f0ee08b4f11ba3e83e3c2526f5dda27
kamal11 <kyzx11@gmail.com>
2019-02-02 19:33:57 +0530

    added button span

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -21,6 +21,15 @@ const CartItem = ({ item, value }) => {
         <span className="d-lg-none">product: </span>
         {price}
       </div>
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+          <div>
+            <span className="btn btn-black mx-1" onClick={() => decrement(id)}>
+              -
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
