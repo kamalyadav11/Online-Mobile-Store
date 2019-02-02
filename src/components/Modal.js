6bdff610ae71e94100aa6f2a5235017d084b22a3
kamal11 <kyzx11@gmail.com>
2019-02-02 15:45:36 +0530

    Added price to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -25,6 +25,7 @@ export default class Modal extends Component {
                         <h5>item added to the cart</h5>
                         <img src={img} className="img-fluid" alt="product" />
                         <h5>{title}</h5>
+                        <h5 className="text-muted">price: $ {price}</h5>
                       </div>
                     </div>
                   </div>
