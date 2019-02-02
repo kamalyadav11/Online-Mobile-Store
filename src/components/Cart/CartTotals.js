edac0f3164624ed9557bb293d0118354dc2a32d3
kamal11 <kyzx11@gmail.com>
2019-02-02 22:10:41 +0530

    client ID problem

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PayPalButton from "./PayPalButton";
+// import PayPalButton from "./PayPalButton";
 
 const CartTotals = ({ value, history }) => {
   const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
@@ -30,11 +30,11 @@ const CartTotals = ({ value, history }) => {
               <span className="text-title">total :</span>
               <strong>$ {cartTotal}</strong>
             </h5>
-            <PayPalButton
+            {/* <PayPalButton
               total={cartTotal}
               clearCart={clearCart}
               history={history}
-            />
+            /> */}
           </div>
         </div>
       </div>
