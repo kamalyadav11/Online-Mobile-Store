7dc875050d1cf537029f70a653d99a4e162d07ef
kamal11 <kyzx11@gmail.com>
2019-02-02 19:25:52 +0530

    rendered cartItem with props

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CartItem from "./CartItem";
 
-const CartList = () => {
+const CartList = ({ value }) => {
+  const { cart } = value;
   return (
-    <div>
-      Hello
-      <CartItem />
+    <div className="container-fluid">
+      {cart.map(item => {
+        return <CartItem key={item.id} item={item} value={value} />;
+      })}
     </div>
   );
 };
