dd4337b21856fdc400e490deb7088898c8156b4f
kamal11 <kyzx11@gmail.com>
2019-02-02 14:46:21 +0530

    added methods in Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,32 +7,34 @@ import { ProductConsumer } from "../context";
 
 export default class Product extends Component {
   render() {
-    const { title, img, price, inCart } = this.props.product;
+    const { id, title, img, price, inCart } = this.props.product;
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
           <ProductConsumer>
-            <div
-              className="img-container p-5"
-              onClick={() => console.log("clicked")}
-            >
-              <Link to="/details">
-                <img src={img} alt="product" className="card-img-top" />
-              </Link>
-              <button
-                className="cart-btn"
-                disabled={inCart ? true : false}
-                onClick={() => console.log("added to cart")}
+            {value => (
+              <div
+                className="img-container p-5"
+                onClick={() => value.handleDetail()}
               >
-                {inCart ? (
-                  <p className="text-capitalize mb-0" disabled>
-                    In Cart
-                  </p>
-                ) : (
-                  <i className="fas fa-cart-plus" />
-                )}
-              </button>
-            </div>
+                <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top" />
+                </Link>
+                <button
+                  className="cart-btn"
+                  disabled={inCart ? true : false}
+                  onClick={() => value.addToCart(id)}
+                >
+                  {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      In Cart
+                    </p>
+                  ) : (
+                    <i className="fas fa-cart-plus" />
+                  )}
+                </button>
+              </div>
+            )}
           </ProductConsumer>
           {/* card-footer */}
           <div className="card-footer d-flex justify-content-between">
