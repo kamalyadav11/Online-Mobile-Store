d0a773145bc1510e1c9926c1fbd3600eb7a64d61
kamal11 <kyzx11@gmail.com>
2019-02-02 15:35:15 +0530

    added id to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -18,7 +18,10 @@ export default class Modal extends Component {
                 <ModalContainer>
                   <div className="container">
                     <div className="row">
-                      <div className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
+                      <div
+                        id="modal"
+                        className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize"
+                      >
                         <h5>item added to the cart</h5>
                       </div>
                     </div>
