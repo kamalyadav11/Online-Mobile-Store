37c8e898d555cfaccd910129710e133f7a8a8185
kamal11 <kyzx11@gmail.com>
2019-02-02 22:02:23 +0530

    organized imports

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
+import { ProductConsumer } from "../../context";
 import Title from "../Title";
 import CartColumns from "./CartColumns";
-import EmptyCart from "./EmptyCart";
-import { ProductConsumer } from "../../context";
 import CartList from "./CartList";
 import CartTotals from "./CartTotals";
+import EmptyCart from "./EmptyCart";
 
 export default class Cart extends Component {
   render() {
