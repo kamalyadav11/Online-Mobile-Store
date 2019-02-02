58337f15cde87455978b9dfbc2a2e35557bcf156
kamal11 <kyzx11@gmail.com>
2019-02-02 15:24:58 +0530

    organized imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import "./App.css";
-import { Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar";
-import ProductList from "./components/ProductList";
-import Details from "./components/Details";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
+import Details from "./components/Details";
 import Modal from "./components/Modal";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
 
 class App extends Component {
   render() {
