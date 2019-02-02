4eadb9e41c71a9ad47c31679d54d472c1f5168c1
kamal11 <kyzx11@gmail.com>
2019-02-02 20:33:39 +0530

    bug fix

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CartTotals = ({ value }) => {
-  const { cartSubtotal, cartTax, cartTotal, clearCart } = value;
+  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
   return (
     <React.Fragment>
       <div className="container">
@@ -19,7 +19,7 @@ const CartTotals = ({ value }) => {
             </Link>
             <h5>
               <span className="text-title">subtotal :</span>
-              <strong>$ {cartSubtotal}</strong>
+              <strong>$ {cartSubTotal}</strong>
             </h5>
             <h5>
               <span className="text-title">tax :</span>
